e71f8c6602a907df6ea6ba5501c9514d91ba865a
Martín Mello Teggia <martinmt139@gmail.com>
2021-06-17 16:51:52 -0300

    Added restrictions to prevent jumping again mid-air

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rigidBody;
     public float thrust = 10.0f;
+    public LayerMask groundLayerMask;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,12 +20,20 @@ public class PlayerController : MonoBehaviour
             Input.GetKeyDown(KeyCode.Space) ||
             Input.GetKeyDown(KeyCode.W))
         {
-            Jump();
+            if (IsOnTheGround())
+            {
+                Jump();
+            }
         }
     }
 
     void Jump()
     {        
         rigidBody.AddForce(Vector2.up * thrust, ForceMode2D.Impulse);
+    }
+
+    bool IsOnTheGround()
+    {
+        return Physics2D.Raycast(transform.position, Vector2.down, 1.0f, groundLayerMask.value);
     }
 }
